066f57ffb58874df531d596eaccbceabbe63ca4f
devlinium-md <manaf6552@gmail.com>
2023-10-03 11:50:48 +0300

    Some small hanges

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyautogui
 
 while True:
     x, y = pyautogui.position()
-    button = pyautogui.locateOnScreen("img.png", confidence=0.5)
+    button = pyautogui.locateOnScreen("img/destroy.png", confidence=0.5)
     pyautogui.click(button)
     pyautogui.moveTo(x, y)
     time.sleep(2)
