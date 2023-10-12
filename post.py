bf8e4b0a9e5f6657fe1ceff25eb5df1901d3b60c
devlinium-md <manaf6552@gmail.com>
2023-10-12 13:13:51 +0300

    Fix

--- post.py
+++ post.py
@@ -1,24 +1,17 @@
-import pyautogui
 import time
-import random
-import sys
+import pyautogui
 
-
-while True:
-    # Start curdor position.
+counter = 0
+time.sleep(5)
+while counter < 10:
     x, y = pyautogui.position()
-    # Post button, used to post the item on aucution.
-    post_button = pyautogui.locateOnScreen("img/post.png", confidence=0.9)
-    # Target to stop the script.
-    finish_target = pyautogui.locateOnScreen("img/post_finished.png", confidence=0.9)
-    if post_button != None and finish_target == None:
-        # Click on post button.
-        pyautogui.click(post_button)
-        # move cursor to the start position.
+    button = pyautogui.locateOnScreen("img/post.png", confidence=0.8)
+    if button is not None:
+        pyautogui.click(button)
         pyautogui.moveTo(x, y)
-        # Simulate human click timeout.
-        time.sleep(random.uniform(0.2, 0.25))
-    # Condition to stop the loop.
-    # See image 'img/post_finished.png'.
-    elif finish_target != None:
-        sys.exit()
+        time.sleep(0.5)
+        button = None
+        counter = 0
+    else:
+        counter += 1
+        time.sleep(1)
